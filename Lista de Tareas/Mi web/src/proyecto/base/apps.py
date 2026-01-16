1c2ac26efe6268c9c6917144878cdb75a9aa5e34
repositorio_prueba <jesusareshdz95@gmail.com>
2026-01-16 10:35:52 +0100

    Fix pytest config and Django app imports

--- Lista de Tareas/Mi web/src/proyecto/base/apps.py
+++ Lista de Tareas/Mi web/src/proyecto/base/apps.py
@@ -1,6 +1,6 @@
-﻿from django.apps import AppConfig
+from django.apps import AppConfig
 
 
 class BaseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "base"
+    name = "proyecto.base"
